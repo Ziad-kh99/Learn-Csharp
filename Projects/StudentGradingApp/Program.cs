cc4aafd32e395f441ece0a5b59462830abb825ed
Ziad <ziadkh99@hotmail.com>
2024-04-19 23:30:28 +0200

    Complete 2nd Version of Student Grading App

--- Projects/StudentGradingApp/Program.cs
+++ Projects/StudentGradingApp/Program.cs
@@ -33,45 +33,158 @@ int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
 int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
 int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
 
-Console.WriteLine($"Sophia: {sophiaSum}");
-Console.WriteLine($"Nicolas: {nicolasSum}");
-Console.WriteLine($"Zahira: {zahirahSum}");
-Console.WriteLine($"Jeong: {jeongSum}");
-
-Console.WriteLine("\n================================\n");
-
 
 //> Calculate Averages:
-
 decimal sophiaScore = (decimal)sophiaSum / currentAssignments;
 decimal nicolasScore = (decimal)nicolasSum / currentAssignments;
 decimal ZahiraScore = (decimal)zahirahSum / currentAssignments;
 decimal jeongScore = (decimal)jeongSum / currentAssignments;
 
-Console.WriteLine($"Sophia: {sophiaScore}");
-Console.WriteLine($"Nicolas: {nicolasScore}");
-Console.WriteLine($"Zahira: {ZahiraScore}");
-Console.WriteLine($"Jeong: {jeongScore}");
 
-Console.WriteLine("\n================================\n");
-
-Console.WriteLine($"Sophia: {sophiaScore} A");
-Console.WriteLine($"Nicolas: {nicolasScore} B");
-Console.WriteLine($"Zahira: {ZahiraScore} B");
-Console.WriteLine($"Jeong: {jeongScore} A");
-
-Console.WriteLine("\n==============[ Final Version ]==================\n");
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine($"Sophia:\t\t{sophiaScore}\tA");
-Console.WriteLine($"Nicolas:\t{nicolasScore}\tB");
+Console.WriteLine($"Sophia:\t\t{sophiaScore}\tA-");
+Console.WriteLine($"Nicolas:\t{nicolasScore}\tB+");
 Console.WriteLine($"Zahirah:\t{ZahiraScore}\tB");
-Console.WriteLine($"Jeong:\t\t{jeongScore}\tA");
+Console.WriteLine($"Jeong:\t\t{jeongScore}\tA-");
 */
 #endregion
 
 
 #region Version 2:
+
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] nicolasScores = { 92, 89, 81, 96, 90, 89 };
+int[] zahirahScores = [90, 85, 87, 98, 68, 89, 89, 89];
+int[] jeongScores = [90, 95, 87, 88, 96, 96];
+int[] andrewScores = { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = { 91, 91, 91, 91, 91, 91, 91 };    
+
+string[] studentNames = ["Sophia", "Nicolas", "Zahirah", "Jeong", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor"];
+
+int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+
+
+Console.WriteLine("Student  \t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+
+    else if (currentStudent == "Nicolas")
+        studentScores = nicolasScores;
+
+
+    else if (currentStudent == "Zahirah")
+        studentScores = zahirahScores;
+
+
+    else if (currentStudent == "Jeong")
+        studentScores = jeongScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+
+    int studentSum = 0;
+    int gradedAssignments = 1;
+    foreach (int score in studentScores)
+    {
+        if (gradedAssignments <= currentAssignments)
+        {
+            studentSum += score;
+        }
+        else
+        {
+            studentSum += score / 10;
+        }
+        gradedAssignments++;
+    }
+
+    decimal currentStudentGrade = (decimal)studentSum / currentAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}:  \t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
+
+
+
+
+
+
+
+
+
 
 #endregion
 
--- Projects/StudentGradingApp/README.md
+++ Projects/StudentGradingApp/README.md
@@ -29,13 +29,16 @@
 - Start with four students. Each student has five exam scores.
 - Each exam score is an integer value, 0-100, where 100 represents 100% correct.
 - A student's overall exam score is the average of their five exam scores.
-- Criteria for extra credit assignments:
-    - Include extra credit assignment scores in the student's scores array.
-    - Extra credit assignments are worth 10% of an exam score (when calculating the final numeric grade).
-    - Add extra credit assignment scores to the student's total exam score before calculating the final numeric grade.
 - Your application needs to automatically assign letter grades based on the calculated final score for each student.
 - Your application needs to output/display each student’s name and formatted grade.
 - Your application needs to support adding other students and scores with minimal impact to the code.
+- Your application needs to accommodate extra credit assignments.
+    - Extra credit scores must be included in the student's scores array.
+    - Extra credit assignments will be worth 10% of an exam score when applied toward the final numeric grade.
+    - Extra credit assignment scores must be added to the student's total exam score before calculating the final numeric grade.
+- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
+    - Your code must detect extra credit assignments based on the number of elements in the student's scores array.
+    - Your code must apply the 10% weighting factor to extra credit assignments before adding extra credit scores to the sum of exam scores.
 
 #### Objectives:
 - Use arrays to store student names and assignment scores.
@@ -44,9 +47,7 @@
 - Use a foreach statement within the outer loop to iterate through the assignment scores array and sum the values.
 - Use an updated algorithm within the outer loop to calculate the average exam score for each student.
 - Use an if-elseif-else construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
-- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
-    - Your code must detect extra credit assignments based on the number of elements in the student's scores array.
-    - Your code must apply the 10% weighting factor to extra credit assignments before adding extra credit scores to the sum of exam scores.
+
 
 
 
