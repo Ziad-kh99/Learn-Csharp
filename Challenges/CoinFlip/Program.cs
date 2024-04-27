013a7cce121ff1b1ad4e77c5495d811591dc1032
Ziad Khaled <ziadkh99@hotmail.com>
2024-04-27 22:58:24 +0300

    Complete Coin Flip Challenge

---
+++ Challenges/CoinFlip/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Random randomNumberGenerator = new Random();
+int flip = randomNumberGenerator.Next(1, 3);
+Console.WriteLine($"Result: {(flip == 1 ? "Heads": "Tails")}");
+
+Console.WriteLine($"Result: {(randomNumberGenerator.Next(1, 3) == 1 ? "Heads": "Tails")}");
