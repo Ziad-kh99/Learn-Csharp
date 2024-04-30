f0737ce00985aea8bbdf826aa3561a010f0578c2
Ziad Khaled <ziadkh99@hotmail.com>
2024-04-30 23:30:20 +0300

    Complete UpdateProblematicCode Challenge

---
+++ Challenges/UpdateProblematicCode/Program.cs
@@ -0,0 +1,48 @@
+﻿#region Version 1 of Code:
+/*
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (var number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+        bool found = true;
+    }
+
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
+*/
+
+#endregion
+
+
+#region Version 2 of Code:
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (var number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
+
+#endregion
