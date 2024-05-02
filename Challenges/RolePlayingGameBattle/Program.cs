c35b69b4b4457e81206e8b3eae2f605c03fa37b5
Ziad Khaled <ziadkh99@hotmail.com>
2024-05-02 11:40:46 +0300

    Complete RolePlayingGameBattle Challenge

---
+++ Challenges/RolePlayingGameBattle/Program.cs
@@ -0,0 +1,42 @@
+﻿Random random = new Random();
+int heroHealth = 10;
+int monsterHealth = 10;
+int attacks;
+int damage;
+
+do
+{
+    attacks = random.Next(1, 11);
+    damage = 0;
+    while (attacks > 0)
+    {
+        monsterHealth--;
+        damage++;
+        attacks--;
+    }
+    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+    if (monsterHealth <= 0)
+    {
+        Console.WriteLine("Hero wins!");
+        break;
+    }
+
+    attacks = random.Next(1, 11);
+    while (attacks > 0)
+    {
+        heroHealth--;
+        damage++;
+        attacks--;
+    }
+    Console.WriteLine($"Hero was damaged and lost {damage} health and now has {monsterHealth} health.");
+    if (heroHealth <= 0)
+    {
+        Console.WriteLine("Monster wins!");
+        break;
+    }
+
+
+}while(heroHealth > 0 && monsterHealth > 0);
+
+
+
---
+++ Challenges/RolePlayingGameBattle/README.md
@@ -0,0 +1,12 @@
+# Role Playing Game Battle:
+## Challenge Roles:
+- Here are the rules for the battle game that you need to implement in your code project:
+    - You must use either the do-while statement or the while statement as an outer game loop.
+    - The hero and the monster will start with 10 health points.
+    - All attacks will be a value between 1 and 10.
+    - The hero will attack first.
+    - Print the amount of health the monster lost and their remaining health.
+    - If the monster's health is greater than 0, it can attack the hero.
+    - Print the amount of health the hero lost and their remaining health.
+    - Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+    - Print the winner.
