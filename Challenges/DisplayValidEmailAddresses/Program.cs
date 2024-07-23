c4528ec1ef4916703cb21dbd194baab1a6139092
Ziad Khaled <ziadkh99@hotmail.com>
2024-07-23 15:37:14 +0300

    Complete DisplayValidEmailAddress Challenge

---
+++ Challenges/DisplayValidEmailAddresses/Program.cs
@@ -0,0 +1,34 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    DisplayAddresses(corporate[i, 0], corporate[i, 1], "contoso.com");
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayAddresses(external[i, 0], external[i, 1]);
+}
+
+
+
+void DisplayAddresses(string firstName, string lastName, string domain = "hayworth.com")
+{
+    string userName = (firstName.Substring(0, 2) + lastName).ToLower();
+    string emailAddress = $"{userName}@{domain}";
+    Console.WriteLine(emailAddress);
+}
+
+
